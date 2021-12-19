9b70b028600097a3d06a684475f86d08412a7fb5
B0218966 <tushar.pardhe@airtel.com>
2021-12-19 19:42:48 +0530

    inital commit

--- node/database.js
+++ node/database.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const data = require("./vaccineData.json");
+const data = require("../vaccineData.json");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 app.get("/data", (req, res) => {
-    if(!data || data.length === 0)throw new Error("Sorry! no data found");
+    if (!data || data.length === 0) throw new Error("Sorry! no data found");
     res.json(data);
 });
 
