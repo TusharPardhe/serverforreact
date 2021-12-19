3a0001e077d0df5db968d129621ff89218c03b8a
B0218966 <tushar.pardhe@airtel.com>
2021-12-19 18:19:21 +0530

    initial commit

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const data = require("./vaccineData.json");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
