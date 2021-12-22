94c6985d0d575f5af7e14d02a4b5784c6c169a24
B0218966 <tushar.pardhe@airtel.com>
2021-12-22 18:21:48 +0530

    Updating vaccine data: 2021-12-22 18:21:44

--- autorun.sh
+++ autorun.sh
@@ -3,7 +3,7 @@
 git='git --git-dir='$PWD'/.git'
 date=$(date '+%Y-%m-%d %H:%M:%S')
 
-python3 /Users/b0218966/Desktop/server/python/script.py
+runipy python/Untitled.ipynb
 $git checkout main
 $git add .
 git commit -m "Updating vaccine data: $date"
